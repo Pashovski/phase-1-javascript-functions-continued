03e24d0f3614411e585103c96f1f8728a63c42ab
Pasha <pashdoctor@gmail.com>
2021-09-02 10:11:46 -0400

    Partially completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function saturdayFun(rollerSkate = "roller-skate"){
+    return (`This Saturday, I want to ${rollerSkate}!`)
+}
+saturdayFun();
+saturdayFun(dogBath = "bathe my dog" );
+
+function mondayWork(office = "go to the office"){
+    return (`This Monday, I will ${office}.`)
+}
+mondayWork();
+mondayWork(workFromHome = "work from home")
+
